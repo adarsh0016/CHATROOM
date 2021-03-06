829b2b1e2079e0fd1e3770d57fecea1e39aab193
adarsh0016 <adarshprakash16@gmail.com>
2021-03-07 00:57:13 +0530

    added comments

--- private_chatroom/client_private.py
+++ private_chatroom/client_private.py
@@ -1,5 +1,4 @@
 import socket
-import pickle    #to convert lists(any object) into bits format to send in transmission.
 from multiprocessing import Process  #for multiprocessing on sending and recieving messages.
 import sys
 
@@ -11,31 +10,31 @@ DISSCONNECT_MESSAGE = "!DISSCONNECT" #for disconnection, dissconnection msg.
 ADDR = (SERVER, PORT)
 CONNECTED = False
 
-def send_friend(client):
-    friend = input("Enter Friend's Name: ")
+def send_friend(client):    #this fuction sends the friend's name to the server.
+    friend = input("Enter Friend's Name: ")    #asks for friend's name to whom the client wants to be connected to.
     friend = friend.encode(FORMAT)
     friend += b" " * (HEADER - len(friend))
-    client.send(friend)
-    friend_confirmation = (client.recv(HEADER)).decode(FORMAT)
+    client.send(friend)     #send the friend's name to the server.
+    friend_confirmation = (client.recv(HEADER)).decode(FORMAT)      #recieves a confirmation that if the friend whit the entered name is connected to the server or not.
     friend_confirmation = friend_confirmation.strip()
-    if friend_confirmation == "not_found":
+    if friend_confirmation == "not_found":      #if the recieved confirmation is 'not_found' then starts the send_friend fuction again.
         print(f"There's no user with the name '{(friend.decode(FORMAT)).strip()}', check the name or tell your friend to connect first!")
         send_friend(client)
         pass
-    elif friend_confirmation == "same_name":
+    elif friend_confirmation == "same_name":        #if the friend name is same as the client's name then also the send_friend fuction is called.
         print(f"You entered your own name!")
         send_friend(client)
         pass
-    elif friend_confirmation == "found":
+    elif friend_confirmation == "found":        #is the recieved message is found, then we let it go on.
         pass
 
-def send_name(client, name):
+def send_name(client, name):    #this function sends the client's name to the server.
     name = name.encode(FORMAT)
     name += b" " * (HEADER - len(name))
     client.send(name)   #send the name of the new client when they join to the server.
-    name_confirmation = (client.recv(HEADER)).decode(FORMAT)
+    name_confirmation = (client.recv(HEADER)).decode(FORMAT)        #receives a confirmation message if the name entered already exists or not.
     name_confirmation = name_confirmation.strip()
-    if name_confirmation == "no":
+    if name_confirmation == "no":       #if the message received is 'no' then the start fuction is called.
         print("This name already exists, try entering another name!")
         start(client)
         pass
@@ -49,7 +48,7 @@ def send(client, msg):
     client.send(message)   #sending the message to the server.
 
     if msg != DISSCONNECT_MESSAGE:
-        print("Me: " + msg)
+        print("Me: " + msg)     #prints the message entered
     else:
         print("DISSCONNECTED FROM THE CHAT!")
         return
@@ -60,10 +59,10 @@ def rec(client):   #for recieving messages.
         if not warning:  #checking if the warning is recieved to check for incomming messages.
             pass
         else:
-            if warning.strip() == "connected":
+            if warning.strip() == "connected":      #if the warning is 'connected' then prints connected.
                 print("Freind connected!")
                 return
-            elif warning.strip() == "dissconnect":
+            elif warning.strip() == "dissconnect":      #if the warning is dissconnected prints dissconnected with the name of the clinet who dissconnected.
                 dissconnector_name = (client.recv(HEADER)).decode(FORMAT)
                 dissconnector_name = dissconnector_name.strip()
                 print(f"{dissconnector_name} was dissconnected!")
@@ -78,19 +77,19 @@ def rec(client):   #for recieving messages.
 
                 print(f"{sender_name}: {msg_rec}")
 
-def invitation(client):
+def invitation(client):     #this fuction deals with the invitation of clients into the chat.
     correct = True
     while correct:
-        command = input("Enter Command ('!invite' to invite your friend, or '!invited' if your friend is inviting you: ")
+        command = input("Enter Command ('!invite' to invite your friend, or '!invited' if your friend is inviting you: ")       #asks if the client wants to invie or is being invited to the chat.
         command_trans = command.encode(FORMAT)
         command_trans += b" " * (HEADER - len(command_trans))
 
-        if command == "!invite":
+        if command == "!invite":        #if the client wants to invite, then it informs the server and calls the send_friend and rec functions.
             client.send(command_trans)
             send_friend(client)
             rec(client)
             correct = False
-        elif command == "!invited":
+        elif command == "!invited":     #if the client wants ti be invited, then it informs the server and starts the rec fuction.
             client.send(command_trans)
             rec(client)
             correct = False
@@ -102,11 +101,11 @@ def start(client):
     send_name(client, name)
     invitation(client)
 
-    Process(target= rec, args= (client,)).start()  #multiprocessing
+    Process(target= rec, args= (client,)).start()  #multiprocessing.
 
     while True:
         msg = input()
-        Process(target = send, args=(client, msg,)).start()
+        Process(target = send, args=(client, msg,)).start()     #multiprocessing.
 
 if __name__ == '__main__':
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- private_chatroom/server_private.py
+++ private_chatroom/server_private.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-import pickle
 
 HEADER = 5084  #it tells the length of the amount of bytes we are going to recieve.
 PORT = 5050   #computer port, usually above 4000 in unused, so we use 5050 for best circmstances.
@@ -12,51 +11,51 @@ DISSCONNECT_MESSAGE = "!DISSCONNECT" #for disconnection, dissconnection msg.
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # server = socket.socket() is how we create a socket, we have to create a socket and combine it with the server. now inside that we define a family of socket which we want, which for IPV$ we use "socket.AF_INET" and "socket.SOCK_STREAM" is the type or method.
 server.bind(ADDR) #here we bind the server(address) to the socket, i.e anything that connects to the address goes to the socket.
 
-conn_list = {}  #connection list
-chat_list = {}
+conn_list = {}  #connection list.
+chat_list = {}  #chatroom list(contains the name of users in the chat room).
 
 
 def invite(conn, name):
     global conn_list
     global chat_list
     global friend_conn
-    friend = conn.recv(HEADER)
+    friend = conn.recv(HEADER)  #recieving friend's name.
     friend = (friend.decode(FORMAT)).strip()
 
     try:
         connection_msg = "connected"
-        if friend == (name.decode(FORMAT)).strip():
+        if friend == (name.decode(FORMAT)).strip():     #so that no one enters there own name in friend's name.
             same_name = "same_name".encode(FORMAT)
             same_name += b" " * (HEADER-len(same_name))
-            conn.send(same_name)
-            invite(conn, name)
+            conn.send(same_name)    #sending client a warning that they have enter their own name.
+            invite(conn, name)      #starting the fuction again to recieve friend's name.
             return
         len_conn_list = 0
         for i in conn_list:
             len_conn_list += 1
-            if conn_list[i] == friend:
+            if conn_list[i] == friend:  #searching if the entered friend's name is connected or not.
                 len_conn_list = 0
                 friend_conn = i
-                connection_msg = "connected"
-        if len_conn_list == len(conn_list):
-            not_found_msg = "not_found".encode(FORMAT)
+                connection_msg = "connected"    #if the name entered is connected to the server 'connected' msg will be sent.
+        if len_conn_list == len(conn_list):     #this means that the name entered is not found in the list i.e. friend is not connected to the server.
+            not_found_msg = "not_found".encode(FORMAT)  #in this case 'not_found' will be transmitted.
             not_found_msg += b" " * (HEADER-len(not_found_msg))
-            conn.send(not_found_msg)
-            invite(conn, name)
+            conn.send(not_found_msg)    #the not_found msg is transmitted.
+            invite(conn, name)      #as its not found the invite function is called again and the whole process of asking friend's name is started again.
             return
         else:
             found_msg = "found".encode(FORMAT)
             found_msg += b" " * (HEADER-len(found_msg))
-            conn.send(found_msg)
+            conn.send(found_msg)    #here found msg is trans mitted to the client.
 
-        chat_list[conn] = [(conn, (name.decode(FORMAT)).strip()), (friend_conn, friend)]
-        chat_list[friend_conn] = [(friend_conn, friend), (conn, (name.decode(FORMAT)).strip())]
+        chat_list[conn] = [(conn, (name.decode(FORMAT)).strip()), (friend_conn, friend)]    #once connected connection details of client and their friend is being added to the chat_list.
+        chat_list[friend_conn] = [(friend_conn, friend), (conn, (name.decode(FORMAT)).strip())]     #continued... it contains {connection_address_client:[(connection_address_client, client_name),(connection_address_friend,friend_name)]}
 
         connection_msg = connection_msg.encode(FORMAT)
         connection_msg += b" " * (HEADER - len(connection_msg))
-        friend_conn.send(connection_msg)
+        friend_conn.send(connection_msg)    #telling the client and their friend that a have been connected by sending them 'connected' message.
         conn.send(connection_msg)
-        print(f"[ROOM CREATED] with {(name.decode(FORMAT)).strip()} and {friend}.")
+        print(f"[ROOM CREATED] with {(name.decode(FORMAT)).strip()} and {friend}.") #prints that a room have been created between the client and their friend.
     except RuntimeError:
         pass
     except ConnectionResetError:
@@ -71,14 +70,14 @@ def handle_client(conn, addr, name):  # this part will handel the individual cli
     connected = True
     while connected:
         try:    #error handeling if someone close the window without sending the dissconnection message.
-            msg_length = (conn.recv(HEADER).decode(FORMAT)).strip() #here it decodes the msg length from byte format.
-            if not msg_length:
+            msg_length = (conn.recv(HEADER).decode(FORMAT)).strip() #here it decodes the msg length from byte format recied from the client.
+            if not msg_length: #if no message is recieved then do nothing.
                 pass
             else:
-                if msg_length == "!invite":
+                if msg_length == "!invite": #if the message recieved is !invite then initiate the invite functioon.
                     invite(conn, name)
 
-                elif msg_length == "!invited":
+                elif msg_length == "!invited": #if message recieved is !invited then do nothing.
                     pass
 
                 else:
@@ -86,22 +85,22 @@ def handle_client(conn, addr, name):  # this part will handel the individual cli
                     msg = conn.recv(msg_length).decode(FORMAT) #here it decodes the msg itself.
                     if not msg:
                         pass
-                    elif msg.strip() == DISSCONNECT_MESSAGE:
+                    elif msg.strip() == DISSCONNECT_MESSAGE:    #if its the dissconnect message then:.
                         print(f"[DISSCONNECTION] Chat ended between {(name.decode(FORMAT)).strip()} and {chat_list[conn][1][1]}.")
                         dissconnect_message = "dissconnect".encode(FORMAT)
                         dissconnect_message += b" " * (HEADER-len(dissconnect_message))
 
-                        friend_conn = chat_list[conn][1][0]
+                        friend_conn = chat_list[conn][1][0]     #gets freind's connection address from the chat_list.
                     
-                        chat_list[conn][1][0].send(dissconnect_message)  
-                        chat_list[conn][1][0].send(name)
-                        conn_list.pop(conn)
+                        chat_list[conn][1][0].send(dissconnect_message)  #sends the client the dissconnection message,
+                        chat_list[conn][1][0].send(name)    #and name of the client who dissconnected.
+                        conn_list.pop(conn) #removing the names of client and their friend from the conn_list and chat_list.
                         conn_list.pop(friend_conn)
                         chat_list.pop(conn)
                         chat_list.pop(friend_conn)
-                        quit()
+                        quit()  #ending the thread here.
                         pass
-                    else:
+                    else:   #if the message is just normal chat.
                         try:
                             print(f"[{addr}] [{conn_list[conn]}] to [{chat_list[conn][1][1]}]: {msg}") #prints the msg with the sender address.
 
@@ -110,38 +109,34 @@ def handle_client(conn, addr, name):  # this part will handel the individual cli
                             message_length = str(message_length).encode(FORMAT)
                             message_length += b' ' * (HEADER - len(message_length))  #converting the msg and its length as shown before in client side code.
 
-                            for i in conn_list:
-                                if i == conn:
-                                    sender_name = conn_list[i]   #getting the sender name from the conn_list.
+                            sender_name = conn_list[conn]   #getting the sender name from the conn_list.
 
-                            for i in chat_list:
-                                if i == conn:
-                                    chat_list[i][1][0].send(message_length)  
-                                    chat_list[i][1][0].send(message)   #sending messages to all the connections(clients) available accept the sender.
-                                    chat_list[i][1][0].send(sender_name.encode(FORMAT))     #sending the name of sender.
+                            chat_list[conn][1][0].send(message_length)  
+                            chat_list[conn][1][0].send(message)   #sending messages to all the connections(clients) available accept the sender.
+                            chat_list[conn][1][0].send(sender_name.encode(FORMAT))     #sending the name of sender.
                         except KeyError:
                             pass
 
         except ConnectionResetError:    #if error happens, which happens in the case when someone closes the window without sending the dissconnection message.
             connected = False    #so that loop ends.
             try:        #error handling when someone dissconnects whithout entering name.
-                print(f"[DISSCONNECTION] Chat ended between {chat_list[conn][0][1]} and {chat_list[conn][1][1]}.")
+                print(f"[DISSCONNECTION] Chat ended between {chat_list[conn][0][1]} and {chat_list[conn][1][1]}.")  #prints that the chat between client and their friend is ended.
                 dissconnect_message = "dissconnect".encode(FORMAT)      #sending the warning for the dissconnnection message.
                 dissconnect_message += b" " * (HEADER-len(dissconnect_message))
 
                 friend_conn = chat_list[conn][1][0]
                     
-                chat_list[conn][1][0].send(dissconnect_message)  
-                chat_list[conn][1][0].send(name)
-                conn_list.pop(conn)
+                chat_list[conn][1][0].send(dissconnect_message)    #sends the client the dissconnection message,
+                chat_list[conn][1][0].send(name)    #and name of the client who dissconnected.
+                conn_list.pop(conn)     #removing the names of client and their friend from the conn_list and chat_list.
                 conn_list.pop(friend_conn)
                 chat_list.pop(conn)
                 chat_list.pop(friend_conn)
-                quit()
+                quit()      #ending the thread here.
                 return
-            except KeyError:    #error handling when someone dissconnects whithout entering name.
+            except KeyError:    #error handling when someone dissconnects whithout entering friend's name.
                 print(f"[DISSCONNECTION] {conn_list[conn]} dissconnected without entering friend's name.")
-                conn_list.pop(conn)
+                conn_list.pop(conn)     #removes the client name from the conn_list.
 
     conn.close()  #closes connection.
 
@@ -153,13 +148,13 @@ def accept_name(conn, addr):
     try:
         for i in conn_list:
             len_conn_list += 1
-            if conn_list[i] == (name.strip()).decode(FORMAT):
-                name_confirmation = "no".encode(FORMAT)
+            if conn_list[i] == (name.strip()).decode(FORMAT):       #checks if the name entered already exists or not.
+                name_confirmation = "no".encode(FORMAT)     #if yes the a a 'no' confirmation message is sent to the client.
                 name_confirmation += b" " * (HEADER-len(name_confirmation))
                 conn.send(name_confirmation)
-                accept_name(conn, addr)
+                accept_name(conn, addr)     #the accept_name function is called to ask a different name this time. 
                 return
-        if len_conn_list == len(conn_list):
+        if len_conn_list == len(conn_list):     #if not then a 'yes' confirmation message is sent.
             name_confirmation = "yes".encode(FORMAT)
             name_confirmation += b" " * (HEADER-len(name_confirmation))
             conn.send(name_confirmation)
